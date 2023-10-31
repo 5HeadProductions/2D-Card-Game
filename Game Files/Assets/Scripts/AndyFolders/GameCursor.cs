7d9291a3e5b17c64bcdd47b796312caed275f159
Tamarindo <idontknowwhattonamethiskhay@gmail.com>
2023-10-30 21:23:18 -0700

    cursor selected
    cursor good

---
+++ Game Files/Assets/Scripts/AndyFolders/GameCursor.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCursor : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+    void Start()
+    {
+        Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    void Update()
+    {
+    }
+}
