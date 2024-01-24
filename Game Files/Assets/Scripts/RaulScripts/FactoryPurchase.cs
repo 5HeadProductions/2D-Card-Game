c16b7fc34b905f7eef2d5c29e37a175a7403ca83
EdenArts <ojedaeden08@gmail.com>
2024-01-23 21:29:10 -0800

    Added the new bucket for the menu where the purchased factories will be placed

--- Game Files/Assets/Scripts/RaulScripts/FactoryPurchase.cs
+++ Game Files/Assets/Scripts/RaulScripts/FactoryPurchase.cs
@@ -20,6 +20,7 @@ public class FactoryPurchase : MonoBehaviour
         //When the factory is purchased we make a purchased factory follow the cursor.
         if (factoryInstance != null)
         {
+            //give the factory instance a separate script that will check to see if it should still follow the mouse.
             Vector3 mousePos = Input.mousePosition;
             mousePos = Camera.main.ScreenToWorldPoint(mousePos);
             mousePos.z = 0;
