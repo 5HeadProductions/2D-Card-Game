389e6de732821b8b68a186dd654d851f9c519ef8
EdenArts <ojedaeden08@gmail.com>
2024-02-04 00:04:25 -0800

    Added a new script for the prefab of cards to hold their information. And finished the ability for the card pool to add new cards.

---
+++ Game Files/Assets/CardInfo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+/*
+ * The purpose of this script is to store the CraftedCard scriptable object.
+ * 
+ * This component should be added to the PreFabs of the crafted cards.
+ */
+public class CardInfo : MonoBehaviour
+{
+    public CraftedCard cardInfo;
+}
--- Game Files/Assets/Scripts/RaulScripts/CardPool.cs
+++ Game Files/Assets/Scripts/RaulScripts/CardPool.cs
@@ -1,32 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
+/*
+ * This is a singleton that takes charge of adding cards to the possible cards
+ * that can be spawned when crafting cards and can increase the chances of the cards
+ * spawning.
+ * 
+ * This script should be attached to the manger of the scene usually an empty gameObject.
+ */
 public class CardPool : MonoBehaviour
 {
-    public List<GameObject> cardsObjects;
-    List<int> chanceArray;
-    public GameObject startingCard;
+    
+    [Header("Card List")]
+    public GameObject bronzeCard;
+    public GameObject silverCard;
 
-    private void Start()
+
+    public static List<GameObject> cardsObjects = new List<GameObject>();
+    public static List<int> chanceArray = new List<int>();
+    public static CardPool instance { get; private set; }
+
+    private void Awake()
     {
-        cardsObjects.Add(startingCard);
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one CardPool in the scene");
+        }
+        instance = this;
+
+        //Initiate the card pool if it has not been initiated yet.
+        if (!cardsObjects.Contains(bronzeCard))
+        {
+            AddGameObject(bronzeCard, 10);
+        }
     }
 
+    /*
+     * Adds a new card gameObject to the pool and adds the specified chance for the card to be drawn.
+     */
     public void AddGameObject(GameObject cardObject, int drawChance)
     {
-        this.cardsObjects.Add(cardObject);
+        cardsObjects.Add(cardObject);
         for (int i = 0; i < drawChance; i++)
         {
-            this.chanceArray.Add(this.cardsObjects.Count - 1);
+            chanceArray.Add(cardsObjects.Count - 1);
         }
     }
 
+    /*
+     * Finds the index of the card and increases chances of drawing by specified amount.
+     */
     public void IncreaseChance(GameObject cardObject, int drawChance)
     {
+        int index = cardsObjects.IndexOf(cardObject);
         for (int i = 0; i < drawChance; i++)
         {
-            this.chanceArray.Add(cardsObjects.IndexOf(cardObject));
+            chanceArray.Add(index);
         }
+    }
+
+    /*
+     * Based on the current chances chooses randomly a number on the chanceArray
+     * then we store the value at that index in the chance array and use that value as the 
+     * index for the cardsObject array.
+     */
+    public GameObject DrawCard()
+    {
+        int index = chanceArray[UnityEngine.Random.Range(0, chanceArray.Count)];
+        return cardsObjects[index];
+    }
+
+    /*
+     * Given a gameObject this function extractes the CraftedCard 
+     * scriptable object from it provided it has a CardInfo component.
+     */
+    public CraftedCard GetCardInfo(GameObject gameObject)
+    {
+        return gameObject.GetComponent<CardInfo>().cardInfo;
     }
 }
